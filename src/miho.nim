f5908912762d78b789db86430663b99335a32417
Nelson Crosby <nelsonc@sourcecomb.com>
2018-09-26 09:19:02 +1200

    Add some basic server setup stuff

--- src/miho.nim
+++ src/miho.nim
@@ -1,12 +1,14 @@
-import asyncdispatch
-import asyncnet
+import asyncnet, asyncdispatch
 
 
 type
   MihoServer* = ref MihoServerObj
   MihoServerObj = object
+    svrSock: AsyncSocket
 
   MihoCommandKind = enum
+    mcEmpty,
+    mcClose,
     mcMoveMouse,
     mcClickMouse,
 
@@ -15,23 +17,57 @@ type
 
   MihoCommand* = object
     case kind: MihoCommandKind
+    of mcEmpty, mcClose:
+      discard
     of mcMoveMouse:
       dx, dy: int
     of mcClickMouse:
       button: MihoMouseButton
 
 
-proc newMihoServer*(): MihoServer =
+proc parseCommand(data: string): tuple[command: MihoCommand, remaining: string] =
+  discard
+
+
+proc newMihoServer*(port: Port, address: string = ""): MihoServer =
   new(result)
+  result.svrSock = newAsyncSocket()
+  result.svrSock.setSockOpt(OptReuseAddr, true)
+  result.svrSock.bindAddr(port, address)
 
 
-proc handleCommand*(miho: MihoServer; cmd: MihoCommand) =
+proc handleCommand(miho: MihoServer; cmd: MihoCommand) =
   case cmd.kind:
+    of mcEmpty:
+      discard
+    of mcClose:
+      echo "closing"
     of mcMoveMouse:
       echo "mouse moved"
     of mcClickMouse:
       echo "mouse clicked"
 
 
-proc serve*(miho: MihoServer): Future[void] {.async.} =
-  discard
+proc handleClient(miho: MihoServer; address: string; client: AsyncSocket) {.async.} =
+  var data = ""
+  while true:
+    data &= await client.recv(1024)
+    while data.len > 0:
+      var command: MihoCommand
+      (command, data) = parseCommand(data)
+      miho.handleCommand(command)
+
+      if command.kind == mcEmpty:
+        break       # This is a special internal kind that signals
+                    # there's not enough data to parse the command
+      elif command.kind == mcClose:
+        client.close()
+        return
+
+
+proc serve*(miho: MihoServer) {.async.} =
+  miho.svrSock.listen()
+
+  while true:
+    let (address, client) = await miho.svrSock.acceptAddr()
+    asyncCheck miho.handleClient(address, client)
