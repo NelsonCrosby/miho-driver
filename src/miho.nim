128723a82234ea55d483fd6fb7afe476ca94c903
Nelson Crosby <nelsonc@sourcecomb.com>
2018-09-26 17:33:45 +1200

    Implement some real simple parsing

--- src/miho.nim
+++ src/miho.nim
@@ -6,18 +6,19 @@ type
   MihoServerObj = object
     svrSock: AsyncSocket
 
+  MihoMouseButton = enum
+    mmbLeft = 0, mmbRight = 1
+
   MihoCommandKind = enum
     mcEmpty,
+    mcRetry,
     mcClose,
     mcMoveMouse,
     mcClickMouse,
 
-  MihoMouseButton = enum
-    mmbLeft = 0, mmbRight = 1
-
   MihoCommand* = object
     case kind: MihoCommandKind
-    of mcEmpty, mcClose:
+    of mcEmpty, mcRetry, mcClose:
       discard
     of mcMoveMouse:
       dx, dy: int
@@ -26,7 +27,36 @@ type
 
 
 proc parseCommand(data: string): tuple[command: MihoCommand, remaining: string] =
-  discard
+  result.remaining = data
+  if data.len == 0:
+    result.command.kind = mcEmpty
+    return
+
+  var pos = 0
+  result.command.kind = MihoCommandKind(data[pos])
+  pos += 1
+
+  case result.command.kind:
+    of mcRetry, mcClose:
+      return
+    of mcEmpty:
+      result.command.kind = mcRetry
+      return
+    of mcMoveMouse:
+      if data.len < (pos + 2):
+        result.command.kind = mcEmpty
+        return
+      result.command.dx = int(data[pos])
+      result.command.dy = int(data[pos + 1])
+      pos += 2
+    of mcClickMouse:
+      if data.len < (pos + 1):
+        result.command.kind = mcEmpty
+        return
+      result.command.button = MihoMouseButton(data[pos])
+      pos += 1
+
+  result.remaining = data.substr(pos)
 
 
 proc newMihoServer*(port: Port, address: string = ""): MihoServer =
@@ -38,7 +68,7 @@ proc newMihoServer*(port: Port, address: string = ""): MihoServer =
 
 proc handleCommand(miho: MihoServer; cmd: MihoCommand) =
   case cmd.kind:
-    of mcEmpty:
+    of mcEmpty, mcRetry:
       discard
     of mcClose:
       echo "closing"
